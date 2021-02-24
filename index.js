abddb4179be7f6d51ec29528e5f09fd7bdb2394b
cubOlson <cub.olson@gmail.com>
2021-02-24 16:59:51 -0500

    Need to run Phase 2 tests

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
+const csrf = require("csurf");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.set("view engine", "pug");
+app.use(cookieParser()); // Adding cookieParser() as application-wide middleware
+app.use(express.urlencoded());
+const csrfProtection = csrf({ cookie: true }); // creating csrfProtection middleware to use in specific routes
+
 
 app.get("/", (req, res) => {
   res.render('index', { users });
+});
+
+app.get("/create", csrfProtection, (req, res) => {
+  res.render('form', { title: "Create Normal User", csrfToken: req.csrfToken() });
 });
 
 const users = [
